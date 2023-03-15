1f254e6c2e17fa1c2a307b934a823c6062a13c8e
Brian Autieri <119701025+Brian-Autieri@users.noreply.github.com>
2023-03-15 16:35:29 -0700

    Update HTML file

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,19 @@
 var startButtonEl = document.querySelector(".start-button");
+var timerEl = document.querySelector(".timer-count");
+
+var timer;
+var secondsLeft = 75;
+
+
+
+startButtonEl.addEventListener("click", function(event){
+    // Start timer
+timer = setInterval(function(){
+    secondsLeft--;
+    console.log(secondsLeft);
+    timerEl.textContent = secondsLeft;
+if (secondsLeft === 0){
+    clearInterval(timer);
+}
+    }, 1000)
+})
