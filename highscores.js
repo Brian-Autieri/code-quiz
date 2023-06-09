48b0e1ea5ad575680a5d2761559dc4f8d5fb8e19
Brian Autieri <119701025+Brian-Autieri@users.noreply.github.com>
2023-06-08 22:41:37 -0700

    Update highscores.js

--- highscores.js
+++ highscores.js
@@ -1,14 +1,38 @@
-var clearHighscoresEl = document.querySelector('clear-highscores');
-var highscoresEl = document.querySelector('highscores-list');
+var submitButtonEl = document.querySelector('.submit-score');
+var initialsEl = document.querySelector('.initials');
+var scoreEl = document.querySelector('.score');
+var highscoresEl = document.querySelector('#highscores-list');
 
-function clearDisplay() {
-    highscoresEl.innerHTML = '';
+submitButtonEl.addEventListener('click', function () {
+    var initials = initialsEl.value.trim();
+    var score = scoreEl.textContent.trim();
+
+    if (initials !== '' && score !== '') {
+        var highscores = JSON.parse(localStorage.getItem('highscores')) || [];
+
+        var newScore = {
+            initials: initials,
+            score: score,
+        };
+
+        highscores.push(newScore);
+        localStorage.setItem('highscores', JSON.stringify(highscores));
+
+
+        initialsEl.value = '';
+
+        window.location.href = 'highscores.html';
+    }
+});
+
+function displayHighscores() {
+    var highscores = JSON.parse(localStorage.getItem('highscores')) || [];
+
+    highscores.forEach(function (score) {
+        var listItemEl = document.createElement('li');
+        listItemEl.textContent = score.initials + ' - ' + score.score;
+        highscoresEl.appendChild(listItemEl);
+    });
 }
 
-function clearHighscores() {
-    localStorage.clear();
-    clearDisplay();
-}
-
-
-clearHighscoresEl.addEventListener('click', clearHighscores);
+displayHighscores();
